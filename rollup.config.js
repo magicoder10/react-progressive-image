4e5256b31d5ce44f3b2c66dc665f87432cdf41db
Harry Liu <byliuyang11@gmail.com>
2019-02-15 23:09:27 -0800

    Add tsconfig for bundling and clean up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-// import babel from 'rollup-plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
@@ -10,8 +9,6 @@ import pkg from './package.json';
 const extensions = [
     '.js', '.jsx', '.ts', '.tsx',
 ];
-
-const name = 'ImageLoader';
 
 export default {
     input: 'src/index.ts',
@@ -40,6 +37,7 @@ export default {
 
         typescript(
             {
+                tsconfig: "tsconfig.bundle.json",
                 rollupCommonJSResolveHack: true,
                 clean: true
             }
